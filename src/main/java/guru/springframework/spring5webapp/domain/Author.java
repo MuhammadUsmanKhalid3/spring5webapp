b0de320bc7eec1e0195a036b9d90b6b5da020281
usman_khalid <usmanochiha11@gmail.com>
2022-02-14 12:46:41 +0500

    jpa workings many to many

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,9 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Set;
 @Entity
 public class Author {
@@ -12,6 +9,7 @@ public class Author {
     private Long id;
     private String firstName;
     private String lastName;
+    @ManyToMany( mappedBy = "authors")
     private Set<Book> books;
 
     public Long getId() {
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,9 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Set;
 @Entity
 public class Book {
@@ -12,6 +9,8 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name="auther_book" ,joinColoumns=@JoinColumn("book_id"), inverseJoinColumns = @JoinColumn("auther_id"))
     private Set<Author> authurs;
     public Book() {
 
